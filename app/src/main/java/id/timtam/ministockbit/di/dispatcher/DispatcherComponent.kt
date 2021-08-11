460a661e2f94fdf8b58d6a6c8a50d66e106f63bf
Timur Haryo <timurharyo00@gmail.com>
2021-08-11 16:35:44 +0700

    [delete] dispatcher component from koin modules

--- app/src/main/java/id/timtam/ministockbit/di/dispatcher/DispatcherComponent.kt
+++
@@ -1,8 +0,0 @@
-package id.timtam.ministockbit.di.dispatcher
-
-import id.timtam.core.dispatcher.DispatcherProviderImpl
-import org.koin.dsl.module
-
-val dispatcherComponent = module {
-    factory { DispatcherProviderImpl() }
-}
