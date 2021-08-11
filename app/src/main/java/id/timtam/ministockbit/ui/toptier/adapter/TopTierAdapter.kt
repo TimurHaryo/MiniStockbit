5db55e23693992e99496aaa1787375e13d6aff29
Timur Haryo <timurharyo00@gmail.com>
2021-08-11 14:42:45 +0700

    [add] paging top tier adapter

---
+++ app/src/main/java/id/timtam/ministockbit/ui/toptier/adapter/TopTierAdapter.kt
@@ -0,0 +1,35 @@
+package id.timtam.ministockbit.ui.toptier.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import id.timtam.core.abstraction.RecyclerViewAdapter
+import id.timtam.core.abstraction.ViewHolder
+import id.timtam.ministockbit.databinding.ItemTierVolumeBinding
+import id.timtam.ministockbit.domain.model.TopTierVolume
+
+class TopTierAdapter : RecyclerViewAdapter<TopTierAdapter.TopTierViewHolder, TopTierVolume>() {
+    private var onClick: ((TopTierVolume) -> Unit)? = null
+
+    override val holderInflater: (LayoutInflater, ViewGroup, Boolean) -> TopTierViewHolder
+        get() = { layoutInflater, viewGroup, b ->
+            TopTierViewHolder(ItemTierVolumeBinding.inflate(layoutInflater, viewGroup, b))
+        }
+
+    override fun onBindViewHolder(holder: TopTierViewHolder, position: Int) {
+        holder.bind(listData[position])
+    }
+
+    fun setOnTierClickListener(action: (TopTierVolume) -> Unit) {
+        this.onClick = action
+    }
+
+    inner class TopTierViewHolder(itemView: ItemTierVolumeBinding) :
+        ViewHolder<ItemTierVolumeBinding, TopTierVolume>(itemView) {
+
+        override fun bind(data: TopTierVolume) {
+            binding.root.setOnClickListener {
+                onClick?.invoke(data)
+            }
+        }
+    }
+}
